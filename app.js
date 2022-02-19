ff0b00426c7ab6ec4aa43f847586d4c2aa0e77cf
Abdullahi Sabriye <abdullahisabriye@gmail.com>
2022-02-19 18:06:47 -0500

    display guessing tiles

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
-const tileDisplay = document.querySelector(`.tile-container`)
-const keyboard = document.querySelector(`.keyboard-container`)
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.keyboard-container')
 
 
 const keys = [
-  'A',
-  'B',
-  'C',
-  'D',
+  'Q',
+  'W',
   'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
   'F',
   'G',
   'H',
-  'I',
   'J',
   'K',
   'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
   'ENTER',
-  'BACKSPACE',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  '<<',
 ]
+
+const guessRows = [
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', '']
+]
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement('div')
+  rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement('div')
+    tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+    tileElement.classList.add('tile')
+    rowElement.append(tileElement)
+  })
+  tileDisplay.append(rowElement)
+})
 
 const handleClick = () => {
   console.log('clicked')
