0848e0b0cddf3fe62cf74706b09df8b21784f3ee
Abdullahi Sabriye <abdullahisabriye@gmail.com>
2022-02-20 17:14:41 -0500

    got letters to display on tiles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.keyboard-container')
 
-
+const wordle = 'HAPPY'
 const keys = [
   'Q',
   'W',
@@ -42,6 +42,9 @@ const guessRows = [
   ['', '', '', '', '']
 ]
 
+let currentRow = 0
+let currentTile = 0
+
 guessRows.forEach((guessRow, guessRowIndex) => {
   const rowElement = document.createElement('div')
   rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
@@ -54,14 +57,25 @@ guessRows.forEach((guessRow, guessRowIndex) => {
   tileDisplay.append(rowElement)
 })
 
-const handleClick = () => {
-  console.log('clicked')
-}
 
 keys.forEach(key => {
   const buttonElement = document.createElement('button')
   buttonElement.textContent = key
   buttonElement.setAttribute('id', key)
-  buttonElement.addEventListener('click', handleClick)
+  buttonElement.addEventListener('click', () => handleClick(key))
   keyboard.append(buttonElement)
 })
+
+const handleClick = (key) => {
+  console.log('clicked', key)
+  addLetter(key)
+}
+
+const addLetter = (letter) => {
+  const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+  tile.textContent = letter
+  guessRows[currentRow][currentTile] = letter
+  tile.setAttribute('data', letter)
+  currentTile++
+  console.log('guessRows', guessRows)
+}
